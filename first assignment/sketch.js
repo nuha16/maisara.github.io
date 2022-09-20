fccfb6bfd6c98c110a2461a66f28d2dcc4192309
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-09-20 11:46:07 -0600

    added chessboard and first assg

---
+++ first assignment/sketch.js
@@ -0,0 +1,19 @@
+// Project Title
+// Nuha Maisara
+// 19/9/22
+//
+// Extra for Experts:
+// make your own shapes
+
+
+/// What I need to do
+// scene interacts with the mouse(shapes follow mouse, change their size according to mouse, change their color according to mouse, etc)
+// scene interact with the keyboard
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
