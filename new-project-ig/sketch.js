32c123f27c8278a73e02c3d8e1cb269342c2c5d6
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-09-28 12:16:06 -0600

    drawing cat on main

--- new-project-ig/sketch.js
+++ new-project-ig/sketch.js
@@ -3,10 +3,13 @@
 // 22/9/22
 
 // ASK!!!
-let Backgroundimg;
+let Backgroundimg, catImg1, catImg2, someTime;
+let isCat = true;
 
 function preload(){
   Backgroundimg = loadImage("Images/Background.jpg");
+  catImg1 = loadImage("Images/cat 1.png");
+  catImg2 = loadImage("Images/cat 2.png");
 }
 
 function setup() {
@@ -15,4 +18,28 @@ function setup() {
 
 function draw() {
   image(Backgroundimg, 0 , 0, windowWidth, windowHeight);
+  drawCat();
 }
+
+function drawCat(){
+  image(catImg1, windowWidth/2.8, windowHeight/2);
+}
+
+// function setup() {
+//   createCanvas(400, 400);
+//   someTime = 2000;
+// }
+
+// function draw() {
+//   if (millis() > someTime) {
+//     isCat = !isCat;
+//     someTime = millis() + 2000;
+//   }
+  
+//   if (isCat) {
+//     background("red");
+//   }
+//   else {
+//     background("black");
+//   }
+// }
