87a98328a16c1e85b0bf94c9df059c9ff40ed955
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-10-04 12:00:06 -0600

    uhh i'll fix it in a bit

--- new-project-ig/sketch.js
+++ new-project-ig/sketch.js
@@ -2,7 +2,7 @@
 // Nuha Maisara
 // 22/9/22
 
-let Backgroundimg, catImg1, catImg2, someTime;
+let Backgroundimg, catImg1, catImg2, startImg1, startImg2, x, y, someTime;
 let isCat1 = true;
 let state = "start screen";
 
@@ -10,20 +10,27 @@ function preload(){
   Backgroundimg = loadImage("Images/Background.jpg");
   catImg1 = loadImage("Images/cat 1.png");
   catImg2 = loadImage("Images/cat 2.png");
+  startImg1 = loadImage("Images/start 1.png");
+  startImg2 = loadImage("Images/start 2.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   someTime = 800;
+  x = 100;
+  y = 200;
 }
 
 function draw() {
   image(Backgroundimg, 0 , 0, windowWidth, windowHeight);
+
   if (state === "start screen"){
     drawCat();
+    image(startImg1, x, y, windowWidth/2, windowHeight/2);
   }
 }
 
+// the blinking cat on the start screen
 function drawCat(){
   if (millis() > someTime) {
     isCat1 = !isCat1;
