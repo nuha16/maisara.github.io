667cc3d924111f05656c50f1ab99a8e1f896e57d
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-10-05 11:18:17 -0600

    collsion demo (help needed)

--- collsion demo/sketch.js
+++ collsion demo/sketch.js
@@ -1,15 +1,26 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Collision demo
 
 
-function setup() {
+let hit = false;
+let theColor;
+
+function setup(){ 
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  rect(200, 200, 100, 150);
+  rect(mouseX, mouseY, 50, 75);
+
+  hit = collideRectRect(200, 200, 100, 150, mouseX, mouseY, 50, 75);
+
+  if (hit) {
+    theColor = "red";
+  }
+  else {
+    theColor = "black";
+  }
+  stroke(theColor);
+  print("colliding?", hit);
 }
