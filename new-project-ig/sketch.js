7fdaa852d759f9399827b77b8deb79c044f21a74
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-10-05 12:17:39 -0600

    more work on start screen

--- new-project-ig/sketch.js
+++ new-project-ig/sketch.js
@@ -11,7 +11,7 @@ function preload(){
   catImg1 = loadImage("Images/cat 1.png");
   catImg2 = loadImage("Images/cat 2.png");
   startImg1 = loadImage("Images/start 1.png");
-  startImg2 = loadImage("Images/start 2.jpg");
+  startImg2 = loadImage("Images/start 2.png");
 }
 
 function setup() {
@@ -25,8 +25,21 @@ function draw() {
 
   if (state === "start screen"){
     drawCat();
-    image(startImg1, windowWidth/2.7, windowHeight/5, windowWidth/4, windowHeight/6);
+    startScreen();
   }
+}
+
+// start button
+function startScreen(){
+  image(startImg1, windowWidth/2.7, windowHeight/5, windowWidth/5, windowHeight/6);
+  if (mouseInsideStart1(startImg1)){
+    image(startImg2, windowWidth/2.7, windowHeight/5, windowWidth/5, windowHeight/6);
+  }
+
+}
+
+function mouseInsideStart1(left, right, top, bottom){
+  return mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom;
 }
 
 // the blinking cat on the start screen
