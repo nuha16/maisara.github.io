cba85efc7c146315c53e2e5e1c626bcabb17591c
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-10-25 12:17:10 -0600

    2d array demo

---
+++ 2d array demo grid/sketch.js
@@ -0,0 +1,47 @@
+// Basic Grid Demo
+// Nuha Maisara
+// 25/10/22
+
+let grid = [[1, 0, 1], [1, 0, 1], [0, 1, 0]];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  displayGrid(grid);
+}
+
+function displayGrid(grid){
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+
+  for (let y=0; y<grid.length; y++){
+    for (let x=0; x<grid.length; x++){
+      if (grid[y][x] === 0){
+        fill("white");
+      }
+      else if (grid[y][x] === 1){
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+}
+
+function mousePressed(){
+  console.log(mouseX, mouseY);
+  let cellWidth = width / grid[0].length;
+  let cellHeight = height / grid.length;
+  
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+
+  if (grid[y][x] === 0){
+    grid[y][x] = 1;
+  }
+  else if (grid[y][x] === 1){
+    grid[y][x] = 0;
+  }
+}
