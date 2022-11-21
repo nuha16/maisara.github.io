54dc7ed78abca2426ec431939143c4117b1040af
nuha16 <114016122+nuha16@users.noreply.github.com>
2022-11-21 11:51:20 -0600

    bnvh

--- button/sketch.js
+++ button/sketch.js
@@ -1,14 +1,48 @@
 // Button OOP challenge
 
+class Button {
+  constructor(x, y, width, height){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.colour = "black";
+    this.hoverColour = "white";
+  }
+
+  display() {
+    if (this.isInside(mouseX, mouseY)){
+      fill(this.hoverColour);
+    }
+    else {
+      fill(this.colour);
+    }
+    rect(this.x, this.y, this.width, this.height);
+  }
+  isInside(x, y){
+    let leftSide = this.x;
+    let rightSide = this.x + this.width;
+    let top = this.y;
+    let bottomSide = this.y + this.height;
+
+    return x > leftSide && x < rightSide && y > top && y < bottomSide;
+
+  }
+}
+let backgroundColour = 220;
+let buttonOne = new Button(200, 300, 200, 100);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(backgroundColour);
+  buttonOne.display();
 }
 
-function button() {
-  
+function mousePressed() {
+  if (buttonOne.isInside(mouseX, mouseY)){
+    backgroundColour = "purple";
+  }
 }
