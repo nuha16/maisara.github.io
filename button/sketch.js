2b6e914145008efa6cc5027b1f0d868e180c4e6e
nuha16 <114016122+nuha16@users.noreply.github.com>
2023-01-19 12:31:49 -0600

    kfbhalkde

--- button/sketch.js
+++ button/sketch.js
@@ -1,49 +1,55 @@
 // Button OOP challenge
 
 class Button {
-  constructor(x, y, width, height){
+  constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.colour = "black";
-    this.hoverColour = "white";
+    this.color = "black";
+    this.hoverColor = "gray";
   }
 
   display() {
-    if (this.isInside(mouseX, mouseY)){
-      fill(this.hoverColour);
+    if (this.isInside(mouseX, mouseY)) {
+      fill(this.hoverColor);
     }
     else {
-      fill(this.colour);
+      fill(this.color);
     }
     rect(this.x, this.y, this.width, this.height);
   }
-  isInside(x, y){
+
+  isInside(x, y) {
     let leftSide = this.x;
     let rightSide = this.x + this.width;
-    let top = this.y;
+    let topSide = this.y;
     let bottomSide = this.y + this.height;
 
-    return x > leftSide && x < rightSide && y > top && y < bottomSide;
-
+    return x > leftSide && x < rightSide &&
+           y > topSide && y < bottomSide;
   }
 }
-let backgroundColour = 220;
-let buttonOne = new Button(200, 300, 200, 100);
-let buttonTwo = new Button(200, 300, 200, 100);
+
+let buttonOne = new Button(200, 300, 150, 75);
+let buttonTwo = new Button(200, 600, 150, 75);
+let backgroundColor = "lightgrey";
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(backgroundColour);
+  background(backgroundColor);
   buttonOne.display();
+  buttonTwo.display();
 }
 
 function mousePressed() {
-  if (buttonOne.isInside(mouseX, mouseY)){
-    backgroundColour = "purple";
+  if (buttonOne.isInside(mouseX, mouseY)) {
+    backgroundColor = "red";
+  }
+  if (buttonTwo.isInside(mouseX, mouseY)) {
+    backgroundColor = "blue";
   }
 }
