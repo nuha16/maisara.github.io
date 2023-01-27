d7fc337a11212d38b9feb14a1c42bb09896d6588
nuha16 <114016122+nuha16@users.noreply.github.com>
2023-01-27 02:24:12 -0600

    button class

--- grid project/sketch.js
+++ grid project/sketch.js
@@ -1,6 +1,29 @@
 // Nuha Maisara
 // Space Shooter Game
 
+class Button {
+  constructor(x, y, width, height, img) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+  }
+
+  display() {
+    image(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  isInside(x, y) {
+    let leftSide = this.x;
+    let rightSide = this.x + this.width;
+    let topSide = this.y;
+    let bottomSide = this.y + this.height;
+
+    return x > leftSide && x < rightSide && y > topSide && y < bottomSide;
+  }
+}
+
 let darkSky, spaceship, rock, littleLego;
 
 let state = "start";
